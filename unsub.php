e0ebf35f70b2013a2f5a637b235e342fd04531c5
AbdulHannan <abdulhannans90@yahoo.com>
2025-05-10 20:58:21 +0530

    Unsub page (#17)
    * unsub wip
    
    * adding unsubscribe page
    
    * updating unsub script

---
+++ unsub.php
@@ -0,0 +1,41 @@
+<?php
+//handle unwanted characters or attacks in the post method
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = htmlspecialchars($_POST["unsub_email"]);
+    if(empty($email)){
+
+    }
+    else {
+        if(store_dnc($email)) {
+            header('Location: index.html');
+        }
+        else {
+            echo " Could not unsubscribe ";
+        }
+    }
+}
+
+function store_dnc($email) {
+    $servername = "localhost";
+    $username = "immehubw_root";
+    $password = "HannanTabrez@123#";
+    $dbname = "immehubw_profiling";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if(!$conn) {
+        die("connection failed: " . mysqli_connect_error());
+
+        exit();
+    }
+
+    $sql = "INSERT INTO `unsub_list`(`unsub_email`) VALUES ('" . $email . "')";
+
+    if (mysqli_query($conn, $sql)) {
+        return true;
+      } else {
+        return false;
+      }
+}
+
+?>
