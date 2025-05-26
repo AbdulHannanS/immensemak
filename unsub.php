4cdddcedfa8035d58de23cdb03f104caabf5dc46
AbdulHannan <abdulhannans90@yahoo.com>
2025-05-27 01:02:23 +0530

    added email unsub notification function

--- unsub.php
+++ unsub.php
@@ -7,12 +7,39 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     else {
         if(store_dnc(trim($email))) {
+            notify_email($email);
             header('Location: unsubscribe_success.html');
         }
         else {
             echo " Could not unsubscribe ";
         }
     }
+}
+
+function notify_email($email) {
+    $to = "awhite@immensetec.com";
+    $subject = "Prospect unsubscribed";
+    
+    $message = "
+    <html>
+    <head>
+    <title>Prospect unsubscribed</title>
+    </head>
+    <body>
+    <h3>The below prospect has chosen to unsubscribe</h3>
+    <h1>" . $email . "</h1>
+    </body>
+    </html>
+    ";
+    
+    // Always set content-type when sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    
+    // More headers
+    $headers .= 'From: noreply@immensetec.com' . "\r\n";
+    
+    mail($to,$subject,$message,$headers);
 }
 
 function store_dnc($email) {
